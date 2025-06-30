af45631d1d1d0b218037845623fadaa2ffeedbc6
Srishankar123 <loknathsrishankar@gmail.com>
2025-06-30 22:24:13 +0530

    Fix: Added escalate_remarks field to Complaint table

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'jaba jaba jaba'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .views import views
     from .auth import auth
